86981763d588050b8614aa205d49767b88748b75
Thomas Clavier <tom@tcweb.org>
2017-05-17 14:37:52 +0200

    Fisrt big step

--- foobarqix.py
+++ foobarqix.py
@@ -1,2 +1,11 @@
 def foobarqix(number):
-    pass
+    asso={3:'Foo', 5: "Bar", 7: "Qix"}
+
+    is_divisible=False
+
+    accumulator = [value for divisor,value in asso.iteritems() if number % divisor == 0 ]
+
+    if len(accumulator) > 0:
+        return "".join(accumulator)
+    return str(number)
+
--- tests.py
+++ tests.py
@@ -4,7 +4,26 @@ from foobarqix import foobarqix
 
 class FooBarQixTests(unittest.TestCase):
 
-    def test_foobarqix(self):
-        self.assertEqual(1, foobarqix(1))
+    def test_should_return_number_when_input_is_not_divisble_by_3_5_7(self):
+        self.assertEqual("1", foobarqix(1))
+        self.assertEqual("4", foobarqix(4))
+
+    def test_should_return_Foo_when_input_is_divisible_by_3(self):
+        self.assertEqual("Foo", foobarqix(6))
+        self.assertEqual("Foo", foobarqix(12))
+
+    def test_should_return_Bar_when_input_is_divisible_by_5(self):
+        self.assertEqual("Bar", foobarqix(10))
+        self.assertEqual("Bar", foobarqix(20))
+
+    def test_should_return_Qix_when_input_is_divisible_by_7(self):
+        self.assertEqual("Qix", foobarqix(14))
+        self.assertEqual("Qix", foobarqix(28))
+
+    def test_should_return_a_combination_of_FooBarQix(self):
+        self.assertEqual("FooQix", foobarqix(3*7))
+        self.assertEqual("BarQix", foobarqix(5*7))
+        self.assertEqual("FooBar", foobarqix(3*5))
+        self.assertEqual("FooBarQix", foobarqix(3*5*7))
         
 
