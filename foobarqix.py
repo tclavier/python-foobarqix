8bcf55609fc5d06175475f6b8633dc5a69d9a860
Thomas Clavier <tom@tcweb.org>
2017-05-30 16:17:46 +0200

    More readable

--- foobarqix.py
+++ foobarqix.py
@@ -1,12 +1,17 @@
+
 def foobarqix(number):
     dico={3:'Foo', 5: "Bar", 7: "Qix"}
 
-    divisible = [ value for divisor,value in dico.iteritems() if number % divisor == 0 ]
-
-    contains = [ dico[int(digit)] for digit in str(number) if int(digit) in dico ]
-
-    result = divisible + contains
-    if len(result) > 0:
+    result = replaceByDivisible(dico, number) + replaceByContains(dico, number)
+    if isDivisibleOrContains(result):
         return "".join(result)
     return str(number)
 
+def replaceByDivisible(dico, number):
+    return [ value for divisor,value in dico.iteritems() if number % divisor == 0 ]
+
+def replaceByContains(dico, number):
+    return [ dico[int(digit)] for digit in str(number) if int(digit) in dico ]
+
+def isDivisibleOrContains(string):
+    return len(string) > 0
